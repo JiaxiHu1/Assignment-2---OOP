799c2b0344ea0acec764f4b029cd107133783256
Jiaxi Hu <Jiaxi_Hu1@baylor.edu>
2022-02-09 14:12:59 -0600

    update

---
+++ CellPhoneClass.py
@@ -0,0 +1,43 @@
+#__manufacture attribute 
+#__model attribute 
+#__retail_price is the price of the retail price 
+
+#__init__ method accept those three atrribute 
+
+#3 other methods 
+#set_manufacture 
+#set_model 
+#set_retail_price 
+
+#3 gets to return the values 
+import random 
+
+class CellPhone: 
+    def __init__(self,ma,mo,p):
+        self.__manufacture = ma
+        self.__model = mo
+        self.__retail_price = p 
+    
+    def set_manufacture(self,ma): 
+        ma = input("Enter the manufacture name:")
+    
+
+    def set_model(self,mo):
+        mo = input("Enter the model:")
+
+    
+    def set_retail_price(self,p):
+        p = float(input("Enter the retail price:"))
+    
+
+    def get_manufacture(self):
+        return self.__manufacture 
+    
+
+    def get_model(self):
+        return self.__model
+
+    
+    def get_retail_price(self):
+        return self.__retail_price 
+
