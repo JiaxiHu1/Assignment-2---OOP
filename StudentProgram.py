a8750b80629346e62d4ae6735c8e2aa4978e542f
Jiaxi Hu <Jiaxi_Hu1@baylor.edu>
2022-02-14 13:56:15 -0600

    c

---
+++ StudentProgram.py
@@ -0,0 +1,22 @@
+import StudentClass as s 
+
+student = input("Please insert you dob: ")
+age = s.age 
+print(age(student))
+
+'''
+if student == 18:
+    print("Your age is:", student)
+    print("Your registration time is Freshmen - 11/10 thru 11/12")
+elif student == 19:
+    print("Your age is:", student)
+    print("Your registration time is Sophomore - 11/7 thru 11/9")
+elif student == 20:
+    print("Your age is:", student)
+    print("Your registration time is Junior - 11/4 thru 11/6")
+elif student == 21:
+    print("Your age is:", student)
+    print("Your registration time is Senior - 11/1 thru 11/3")
+else: 
+    print("The age you entered is not valid. Please enter another age.")
+'''
