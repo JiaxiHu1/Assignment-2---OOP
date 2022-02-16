a7b7ce9d7c8af973492f8c6f3673bbd4bd088374
Jiaxi Hu <Jiaxi_Hu1@baylor.edu>
2022-02-15 22:16:35 -0600

    hw

---
+++ CarClass.py
@@ -0,0 +1,29 @@
+class car:
+    def __init__(self,yr,mk,s):
+        self.__year_model = yr
+        self.__make = mk 
+        self.__speed = 0
+    
+    def set_year(self,yr):
+        self.__year_model = yr 
+    
+    def set_make(self,mk):
+        self.__make = mk 
+
+    def accelerate(self,s):
+        self.__speed += 5 
+    
+    def brake(self,s):
+        self.__speed -= 5 
+    
+    def get_year_model(self,yr):
+        return self.__year_model
+    
+    def get_make(self,mk):
+        return self._make 
+    
+    def get_speed(self,s):
+        return self.__speed
+    
+
+
---
+++ CarProgram.py
@@ -0,0 +1,12 @@
+import CarClass as c 
+
+yr = int(input("Please input the car's year model:"))
+
+mk = input("Please input the make of the car:")
+
+s = float(input("Please input the speed of the car:"))
+
+print("In Accelerate mode, the speed will be")
+
+for s in range(5): 
+ s += c.car(accelerate())
