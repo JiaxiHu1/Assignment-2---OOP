a74930406633cac63f34dffa5c6140d3c2aa04c3
Jiaxi Hu <Jiaxi_Hu1@baylor.edu>
2022-02-15 21:22:23 -0600

    update

--- CellPhone.py
+++ CellPhone.py
@@ -1,5 +1,18 @@
 import CellPhoneClass as cp 
 
-print("the manufacture for the phone is", CellPhone.get_manufacture())
-print(cp.get_model())
-print(cp.get_retail_price())
+
+manufacture =  input('Please enter the name of the manufacturer: ')
+model =  input('Please enter the name of the model: ')
+price = float(input('Please enter the retail price: ')) 
+
+#when call we have to call the 
+# this file was imported from .class 
+#-- which file is it from and which class is it from 
+
+cell = cp.CellPhone(manufacture,model,price)
+
+
+print("the manufacture for the phone is", cell.get_manufacture())
+print("the model of the phone is",cell.get_model())
+print("the price for the phone is $", cell.get_retail_price())
+
